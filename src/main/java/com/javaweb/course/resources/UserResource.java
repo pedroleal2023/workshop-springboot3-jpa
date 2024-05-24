b187fd5384b13d9549de57a79d8d434fd03af240
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-05-24 10:58:12 -0300

    User entity and resource

---
+++ src/main/java/com/javaweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.javaweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "777", "123");
+	return ResponseEntity.ok().body(u);
+    }
+}
