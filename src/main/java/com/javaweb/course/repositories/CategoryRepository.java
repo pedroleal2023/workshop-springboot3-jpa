55360bf3653e6ab64efdc941fa054f97bf1e8254
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-07-07 15:40:51 -0300

    Category

---
+++ src/main/java/com/javaweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.javaweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javaweb.course.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
