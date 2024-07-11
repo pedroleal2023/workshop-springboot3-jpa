a6c956beae61afce48bb6046ea1fa65bb20e125e
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-07-11 10:19:47 -0300

    Exception Handling - update

--- src/main/java/com/javaweb/course/services/UserService.java
+++ src/main/java/com/javaweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.javaweb.course.repositories.UserRepository;
 import com.javaweb.course.services.exceptions.DataBaseException;
 import com.javaweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,11 +45,16 @@ public class UserService {
  }
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+	    } catch (EntityNotFoundException e) {
+	       throw new ResourceNotFoundException(id);
+	    }
 	}
-
+	
+	
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
